9c25dff2cd783a6b2c947cb7980806ff61276db2
Muse You Could Use <museyoucoulduse@gmail.com>
2018-10-18 13:07:47 +0200

    Add minlength and trim to string properties in Blog model

--- models/shoutOutModel.js
+++ models/shoutOutModel.js
@@ -4,9 +4,9 @@ const Schema = mongoose.Schema;
 // no comments as they might obstruct the law
 // delete after 1 hour to comply with EU proposed law
 var blogSchema = new Schema({
-  title:  { type: String, default: 'Lots of lulz' },
-  author: { type: String, default: 'Anonymous' },
-  body:   { type: String, required: true },
+  title:  { type: String, default: 'Lots of lulz', minlength: 1, trim: true },
+  author: { type: String, default: 'Anonymous', minlength: 1, trim: true },
+  body:   { type: String, required: true, minlength: 1, trim: true },
   createdAt: { type: Date, default: Date.now, expires: 60 },
 });
 
